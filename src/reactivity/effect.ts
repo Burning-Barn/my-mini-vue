cbe4e5f30acf58343d92f4b100d7f94622acff72
Burning-Barn <1746657548@qq.com>
2023-08-16 21:30:02 +0800

    commit2 effect_stop onstop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,6 +1,11 @@
+import { extend } from "./shared/index"
+
 // class reactiveEffect 
 class reactiveEffect {
     private _fn
+    deps: Array<Set<any>> = []
+    active = true
+    onStop?: ()=> void
 
     constructor(fn, public scheduler?) {
         this._fn = fn
@@ -11,6 +16,23 @@ class reactiveEffect {
         activeEffect = this
         return this._fn()
     }
+
+    stop() {
+        // 优化stop功能，多次stop，只出发一次
+        if(this.active) {
+            cleanupEffect(this)
+            this.active = false
+            if(this.onStop) {
+                this.onStop()
+            }
+        }
+    }
+}
+
+function cleanupEffect(effect) {
+    effect.deps.forEach((dep) => {
+        dep.delete(effect)
+    })
 }
 
 const targetMap = new Map()
@@ -26,7 +48,10 @@ export function track(target, key) {
         depMap = new Set()
         depsMap.set(key, depMap)
     }
+    if(!activeEffect) return
     depMap.add(activeEffect)
+    // 反向记录，如stop的时候，用activeEffect删除depMap里的它
+    activeEffect.deps.push(depMap)
 }
 
 export function trigger(target, key) {
@@ -41,10 +66,22 @@ export function trigger(target, key) {
     }
 }
 
+export function stop(runner) {
+    runner.effect.stop()
+}
+
 let activeEffect
 export function effect(fn, options:any={}) {
     const scheduler = options.scheduler
     const _effect = new reactiveEffect(fn, scheduler)
+    // options
+    // Object.assign(_effect, options)
+    extend(_effect, options)
+    _effect.onStop = options.onStop
+    
     _effect.run()
-    return _effect.run.bind(_effect)
+
+    const _runner: any = _effect.run.bind(_effect)
+    _runner.effect = _effect
+    return _runner
 }
---
+++ src/reactivity/shared/index.ts
@@ -0,0 +1,4 @@
+export function extend(a, b) {
+     
+    return Object.assign(a, b)
+}
